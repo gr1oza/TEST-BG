4aac347d11e1ed627f970906dec438c18c18642a
gr1oza <grozaalex912@yahoo.com>
2024-09-23 15:15:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["made by gr1oza", "dsc.gg/angelscommunity"];
+const statusMessages = ["made by gr1oza", "dsc.gg/communityofangels"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
